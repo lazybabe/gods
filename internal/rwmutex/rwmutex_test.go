e7b4ce66af98ee05c7960dd7683b7caaf1e3fbd2
Gin <qinyuguang@gmail.com>
2023-01-28 01:28:47 +0800

    feat: set structure

---
+++ internal/rwmutex/rwmutex_test.go
@@ -0,0 +1,54 @@
+package rwmutex_test
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gmeasure"
+
+	"github.com/qinyuguang/gods/internal/rwmutex"
+)
+
+func TestRwMutex(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Mutex Suite")
+}
+
+var _ = Describe("RwMutex", func() {
+	It("Benchmark", Serial, func() {
+		safeLock := rwmutex.New(true)
+		unsafeLock := rwmutex.New(false)
+
+		experiment := gmeasure.NewExperiment("LockUnlock")
+		AddReportEntry(experiment.Name, experiment)
+		experiment.Sample(func(idx int) {
+			experiment.MeasureDuration("SafeLock", func() {
+				safeLock.Lock()
+				defer safeLock.Unlock()
+			})
+			experiment.MeasureDuration("UnsafeLock", func() {
+				unsafeLock.Lock()
+				defer unsafeLock.Unlock()
+			})
+			experiment.MeasureDuration("SafeRLock", func() {
+				safeLock.RLock()
+				defer safeLock.RUnlock()
+			})
+			experiment.MeasureDuration("UnsafeRLock", func() {
+				unsafeLock.RLock()
+				defer unsafeLock.RUnlock()
+			})
+		}, gmeasure.SamplingConfig{N: 1e7, Duration: 10 * time.Second})
+
+		safeLockMedian := experiment.GetStats("SafeLock").DurationFor(gmeasure.StatMedian)
+		unsafeLockMedian := experiment.GetStats("UnsafeLock").DurationFor(gmeasure.StatMedian)
+		safeRLockMedian := experiment.GetStats("SafeRLock").DurationFor(gmeasure.StatMedian)
+		unsafeRLockMedian := experiment.GetStats("UnsafeRLock").DurationFor(gmeasure.StatMedian)
+		Expect(safeLockMedian).To(BeNumerically("<", 150*time.Nanosecond))
+		Expect(unsafeLockMedian).To(BeNumerically("<", time.Nanosecond))
+		Expect(safeRLockMedian).To(BeNumerically("<", 50*time.Nanosecond))
+		Expect(unsafeRLockMedian).To(BeNumerically("<", time.Nanosecond))
+	})
+})
